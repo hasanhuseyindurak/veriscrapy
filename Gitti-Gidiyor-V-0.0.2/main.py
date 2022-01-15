cf32ef0367934276ef226d51f780ad8ea21616b3
Hasan Hüseyin Durak <97699635+hasanhuseyindurak@users.noreply.github.com>
2022-01-15 13:36:00 +0300

    Update main.py

--- Gitti-Gidiyor-V-0.0.2/main.py
+++ Gitti-Gidiyor-V-0.0.2/main.py
@@ -18,52 +18,87 @@ class Uygulama():
         print("===================================== GİTTİ GİDİYOR ===================================")
         print("=======================================================================================")
         for i in range(len(self.gittigidiyorliste)):
-            uruns_ad = self.gittigidiyorliste[i][0]
-            uruns_fiyat = self.gittigidiyorliste[i][1]
+            uruns_ad = self.gittigidiyorliste[i][0].replace('\n','').strip()
+            uruns_fiyat = self.gittigidiyorliste[i][1].replace('\n','').strip()
             print(f"Ürün Adı : {uruns_ad} - Ürün Fiyatı : {uruns_fiyat}")
         print("=======================================================================================")
 
         time.sleep(2)
 
-        self.n11()
+        print("")
+        print("")
+
+        self.n11(boslukdoldur)
 
         print("=======================================================================================")
         print("===================================== N11 =============================================")
         print("=======================================================================================")
         for i in range(len(self.n11liste)):
-            urunss_ad = self.n11liste[i][0]
-            urunss_fiyat = self.n11liste[i][1]
+            urunss_ad = self.n11liste[i][0].replace('\n','').strip()
+            urunss_fiyat = self.n11liste[i][1].replace('\n','').strip()
             print(f"Ürün Adı : {urunss_ad} - Ürün Fiyatı : {urunss_fiyat}")
         print("=======================================================================================")
 
-    def n11(self):
+    def n11(self,urladress):
         self.n11liste = []
-        self.aranacak_sey = input("Aranacak ürün ismi giriniz : ")
-        boslukdoldur = self.aranacak_sey.replace(' ', '+')
-        url = f"https://www.n11.com/arama?q={boslukdoldur}"
-        response = requests.get(url)
-        html_icerigi = response.content
-        soup = BeautifulSoup(html_icerigi, "html.parser")
-        div = soup.find('div', {'class': 'content'})
-        div2 = div.find('div', {'class' : 'container'})
-        div3 = div2.find('div', {'class' : 'listingHolder'})
-        div4 = div3.find('div', {'class' : 'productArea'})
-        div5 = div4.find_all('div', {'class' : 'columnContent'})
-        for i in div5:
-            # bu bölüm düzeltilecek
-            urun_adi1 = i.find('div', {'class' : 'pro'}).find('h3', {'class' : 'productName'})
-            urun_adi = urun_adi1.text
-            print(urun_adi)
-            urn_fiat1 = i.find('div', {'class' : 'proDetail'}).find('span' , {'class' : 'newPrice cPoint priceEventClick'})
-            urn_fiat = urn_fiat1.text
-            print(urn_fiat)
-            self.n11liste.append([urun_adi, urn_fiat])
+        boslukdoldur = urladress.replace(' ', '+')
+        sduzenleme = boslukdoldur.replace('ş','%C5%9F')
+        try:
+            url = f"https://www.n11.com/arama?q={sduzenleme}"
+            response = requests.get(url)
+            html_icerigi = response.content
+            soup = BeautifulSoup(html_icerigi, "html.parser")
+            div = soup.find('div', {'class': 'content'})
+            div2 = div.find('div', {'class' : 'container'})
+            div3 = div2.find('div', {'class' : 'listingHolder'})
+            div4 = div3.find('div', {'class' : 'productArea'})
+            div5 = div4.find('section', {'class' : 'group listingGroup resultListGroup import-search-view'})
+            try:
+                catalog = div5.find('div', {'class': 'catalogView'})
+                ul = catalog.find('ul', attrs={'class': 'clearfix'})
+            except AttributeError:
+                catalog = div5.find('div', {'class': 'listView'})
+                ul = catalog.find('ul')
 
-    def gittigidiyor(self,boslukdoldur):
+            ulli = ul.find_all('li',{'class':'column'})
+            for i in ulli:
+                div6 = i.find('div', {'class' : 'pro'})
+                h3 = div6.find('h3',{'class':'productName'})
+                urun_adi = h3.text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
+                div7 = i.find('div',{'class':'proDetail'})
+                span = div7.find('span',{'class':'newPrice cPoint priceEventClick'})
+                urun_fiyati = span.text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
+                self.n11liste.append([urun_adi,urun_fiyati])
+
+        except Exception as e:
+            boslukdoldur = urladress.replace('+', '+')
+            sduzenleme = boslukdoldur.replace('ş', '%C5%9F')
+            url = f"https://www.n11.com/{sduzenleme}?q={sduzenleme}&ref=auto"
+            response = requests.get(url)
+            html_icerigi = response.content
+            soup = BeautifulSoup(html_icerigi, "html.parser")
+            div = soup.find('div', {'class': 'content'})
+            div2 = div.find('div', {'class': 'container'})
+            div3 = div2.find('div', {'class': 'listingHolder'})
+            div4 = div3.find('div', {'class': 'productArea'})
+            div5 = div4.find('section', {'class': 'group listingGroup resultListGroup import-search-view'})
+            catalog = div5.find('div', {'class': 'catalogView'})
+            ul = catalog.find('ul')
+            ulli = ul.find_all('li', {'class': 'column'})
+            for i in ulli:
+                div6 = i.find('div', {'class': 'pro'})
+                h3 = div6.find('h3', {'class': 'productName'})
+                urun_adi = h3.text.lstrip('\n').lstrip('').rstrip('\n').rstrip().strip('\n')
+                div7 = i.find('div', {'class': 'proDetail'})
+                span = div7.find('span', {'class': 'newPrice cPoint priceEventClick'})
+                urun_fiyati = span.text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n')
+                self.n11liste.append([urun_adi, urun_fiyati])
+
+    def gittigidiyor(self,urladress):
         self.gittigidiyorliste = []
 
         try:
-            url = f"https://www.gittigidiyor.com/arama/?k={boslukdoldur}&qm=1"
+            url = f"https://www.gittigidiyor.com/arama/?k={urladress}&qm=1"
             response = requests.get(url)
             html_icerigi = response.content
             soup = BeautifulSoup(html_icerigi, "html.parser")
@@ -77,12 +112,12 @@ class Uygulama():
             ul = div7.find('ul')
             ulli = ul.find_all('li', {'class' : 'list-item'})
             for i in ulli:
-                urunadi = i.find('h3').text.strip('\n').lstrip('\n').lstrip('').rstrip('\n').rstrip('')
-                urunfiyati = i.find('span',{'class' : 'buy-price'}).text.lstrip('\n').lstrip('').rstrip('\n').rstrip('')
+                urunadi = i.find('h3').text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
+                urunfiyati = i.find('span',{'class' : 'buy-price'}).text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
                 self.gittigidiyorliste.append([urunadi,urunfiyati])
 
         except Exception as e:
-            boslukdoldur = self.aranacak_sey.replace(' ', '%20')
+            boslukdoldur = urladress.replace('+', '%20')
             url = f"https://www.gittigidiyor.com/{boslukdoldur}?k={boslukdoldur}&qm=1"
             response = requests.get(url)
             html_icerigi = response.content
@@ -97,8 +132,8 @@ class Uygulama():
             ul = div7.find('ul')
             ulli = ul.find_all('li', {'class': 'list-item'})
             for i in ulli:
-                urunadi = i.find('h3').text.lstrip('\n').lstrip('').rstrip('\n').rstrip('')
-                urunfiyati = i.find('span', {'class': 'buy-price'}).text.lstrip('\n').lstrip('').rstrip('\n').rstrip('')
+                urunadi = i.find('h3').text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
+                urunfiyati = i.find('span', {'class': 'buy-price'}).text.lstrip('\n').lstrip().rstrip('\n').rstrip().strip('\n').strip()
                 self.gittigidiyorliste.append([urunadi, urunfiyati])
 
         # df = pd.DataFrame(liste,columns = ["Ürün Adı","Fiyat"])
